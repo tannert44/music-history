5531452761afe5b7cb035ae5d991d57c102de50f
Tanner Terry <tannert8155@gmail.com>
2015-07-16 12:57:59 -0500

    Flexin

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var songs = [];
-var song_div = document.getElementById("song-info-wrapper");
-
+var song_div = document.getElementById("song-info-content");
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -8,27 +7,11 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-for(var i = 0; i < songs.length; i++) {
-  var addMusicFront = prompt("Type Song to add to Front or type exit to quit: ");
-  var addMusicBack = prompt("Type Song to add to End or type exit to quit: ");
-  
-  if(addMusicFront.toLowerCase() === "exit"){
-    break;
-  }else {
-    songs.unshift(addMusicFront);
-  }
-  
-  if(addMusicBack.toLowerCase() === "exit"){
-    break;
-  }else {
-    songs.push(addMusicBack);
-  }
-  
-}
+songs.push("My Dick - by Mickey Avalon on the Album Dicks Like Jesus");
+songs.unshift("East to West - by Casting Crowns on the Album Praise You In the Storm");
 
 for(var i = 0; i < songs.length; i++) {
   songs[i] = songs[i].replace(">", "-");
   songs[i] = songs[i].replace(/[(*!@]/g, "");
-  song_div.innerHTML += "<span class='song-info'>"+ songs[i] + "</span>"
+  song_div.innerHTML += "<span class='song-info'>" + songs[i] + "</span>";
 }
-
